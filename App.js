88d8f5d7a73a4abc56aa8407d85c5eb0735846b6
unknown <leomoyano7@gmail.com>
2020-06-06 12:37:54 -0300

    first commit

--- App.js
+++ App.js
@@ -1,15 +1,45 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Modal } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MapView style={styles.map}/>
+      <Modal
+       animationType='slide'
+       transparent={true}
+       visible={true}>
+      <View style={styles.center}>
+        <View style={styles.modalView}>
+          <Text> 😁 </Text>
+        </View>
+      </View>
+      </Modal>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView:{
+    backgroundColor: '#fff',
+    borderRadius: 4,
+    padding: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    }
+  },
+  map: {
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
